11787c9f05b98d23b91f383b590f3d5b50fde167
“Mikegit config --global user.email “mikecardona2015@gmail.com <mikecardona2015@gmail.com>
2015-12-18 20:52:20 -0800

    Added associations

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218042645) do
+ActiveRecord::Schema.define(version: 20151218233334) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
 end
